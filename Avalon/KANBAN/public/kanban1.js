04ec9d43b615c635148e19f0167267c1a0b7d7d8
Eduardo <eduardo.cereza@outlook.com>
2025-04-02 15:22:25 -0300

    comentando o q eu fiz

--- Avalon/KANBAN/public/kanban1.js
+++ Avalon/KANBAN/public/kanban1.js
@@ -1,4 +1,6 @@
 //Pega as divs com a classe column
+//Criei variaveis para podermos pegar elementos do HTML que iremos manipular. As funcoes querySelectorAll pegam todos 
+// os elementos que possuem a classe passada como parametro e retorna um array com todos os elementos encontrados.
 const modal = document.querySelectorAll('.modal'); //janela onde adicionamos tarefas
 const addTaskButton = document.querySelectorAll('.add-task'); //botao de add nova tarefa
 const todolist = document.querySelectorAll('.todo'); //container onde as tarefas são adicionadas
