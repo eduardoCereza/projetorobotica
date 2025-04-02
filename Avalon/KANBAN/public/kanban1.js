f2cb3b1da6ecedce6a00b6d9116d44c17a6a8473
Eduardo <eduardo.cereza@outlook.com>
2025-04-02 15:16:59 -0300

    editando nomes para eu poder me orientar

--- Avalon/KANBAN/public/kanban1.js
+++ Avalon/KANBAN/public/kanban1.js
@@ -1,5 +1,5 @@
 //Pega as divs com a classe column
-const modal = document.querySelectorAll('.modal'); //janela onde adicionamos tarefas
+const jdT = document.querySelectorAll('.janeladeTarefa'); //janela onde adicionamos tarefas
 const addTaskButton = document.querySelectorAll('.add-task');
 const todolist = document.querySelectorAll('.todo');
 
